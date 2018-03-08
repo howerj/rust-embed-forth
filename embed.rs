eb09c6b0731b60dc04d03d32ef38f36034fdf573
Richard James Howe <howe.r.j.89@gmail.com>
2018-03-08 19:37:08 +0000

    Unit tests, default image, documentation
    More documentation has been added and the start of a unit test framework, the
    project is improving in quality. A default image has been added to 'eforth.rs',
    this should not have to be changed often (or at all).
    
    The interface to the virtual machine has also been improved.

--- embed.rs
+++ embed.rs
@@ -1,22 +1,62 @@
 #![crate_name = "embed"]
 
+//use std::default::Default;
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
+mod eforth;
 
 /// * `CORE_SIZE` is the total number of cells addressable by the virtual machine
-pub const CORE_SIZE: usize = 0x8000;
+const CORE_SIZE: usize = 0x8000;
 /// * `SP0` is the starting point of the variable stack
-pub const SP0: u16 = 0x2200;
+const SP0: u16 = 0x2200;
 /// * `RP0` is the starting point of the return stack
-pub const RP0: u16 = 0x7fff;
+const RP0: u16 = 0x7fff;
+
+/// `fputc` writes a single character of output to a file, and returns
+/// all bits set on an error. It emulates the C function of the same name,
+/// and is not a recommended way to output data in Rust, but is required for
+/// the VM.
+///
+/// # Arguments
+///
+/// * `output`  - Output stream to write to
+/// * `t`       - Single byte to write
+///
+/// # Returns
+///
+/// This function returns `t` on success and `0xffff` on error
+///
+fn fputc(output: &mut Write, t: u8) -> u16 {
+	let u: [u8; 1] = [t as u8];
+	if 1 == output.write(&u).unwrap() { t as u16 } else { 0xffff }
+}
+
+/// `fputc` gets a single character from an input stream, like the C function
+/// with the same name, it returns all bits set (-1) on error. This is not a
+/// very idiomatic way of doing things from a Rust point of view, but this
+/// function is used by the virtual machine to get input, and it expects
+/// errors to be signaled in the message.
+///
+/// # Arguments
+///
+/// `input` - Input stream to read from
+///
+/// # Returns
+///
+/// This function returns a single byte on success in the lower half a
+/// 16-bit value, and all bits set (or `0xffff`) on failure.
+fn fgetc(input: &mut Read) -> u16 {
+	let mut u: [u8; 1] = [0];
+	if 1 == input.read(&mut u).unwrap() { u[0] as u16 } else { 0xffff }
+}
 
 /// # Embed Virtual Machine in Rust
 ///
 /// * LICENSE:    MIT
 /// * AUTHOR:     Richard James: Howe
 /// * COPYRIGHT:  Richard James Howe (2018)
-/// * CONTACT:    howe.r.j.89@gmail.com
+/// * CONTACT:    <howe.r.j.89@gmail.com>
 /// * REPOSITORY: <https://github.com/howerj/rust-embed-forth>
 ///
 /// This project implements a 16-bit dual stack virtual machine (VM) tailored to
@@ -27,24 +67,68 @@ pub const RP0: u16 = 0x7fff;
 /// The original C VM is available at <https://github.com/howerj/embed>, along
 /// with more up to date VM images.
 /// 
-/// TODO: Implement Index trait for u16?
-/// TODO: Add a default image in here for completeness sake
-/// TODO: Add tests, not just documentation examples
+/// * TODO: Implement Index trait for u16?
+/// * TODO: Add tests, not just documentation examples
 pub struct VM {
-	/// `trace` can be set true to enable logging, logging is very verbose
-	pub trace: bool, 
+	/// `tracing` can be set true to enable logging, logging is very verbose
+	tracing: bool,
+	/// `count` is the number instructions executed so far, it is only updated
+	/// if tracing is on.
+	count: u64,
 	/// The virtual machine has minimal state, a program counter (`pc`),
 	/// a return stack pointer `rp`, a data stack pointer `sp` and a top
 	/// of stack pointer `t`.
 	pc: u16, rp: u16, sp: u16, t: u16, 
-	/// `core` contains the program, data, and both stacks
-	core: [u16; CORE_SIZE] }
+	/// `core` contains the program, data, and both stacks which index
+	/// into `core` with `rp` and `sp`
+	//#[derive(Copy, Clone)]
+	core: [u16; CORE_SIZE] 
+}
 
+// impl Default for VM {
+// 	fn default() -> Self {
+// 		let mut r = VM { tracing: false, count: 0, pc: 0, rp: RP0, sp: SP0, t: 0, core: [0; CORE_SIZE] };
+// 		for i in 0..eforth::EFORTH_CORE.len() {
+// 			r.core[i] = eforth::EFORTH_CORE[i];
+// 		}
+// 		r
+// 	}
+// }
+ 
 impl VM {
-	pub fn new() -> VM { 
-		VM {
-			trace: false, pc: 0, rp: RP0, sp: SP0, t: 0, core: [0; CORE_SIZE]
+
+	/// `new` constructs a new virtual machine image that can be passed to `run`
+	/// straight away, as the program memory is copied from a default image
+	/// that contains a eForth interpreter.
+	///
+	pub fn new() -> Self { 
+		let mut r = VM { tracing: false, count: 0, pc: 0, rp: RP0, sp: SP0, t: 0, core: [0; CORE_SIZE] };
+
+		for i in 0..eforth::EFORTH_CORE.len() {
+			r.core[i] = eforth::EFORTH_CORE[i];
 		}
+		r
+	}
+
+	/// `reset` sets the VMs registers back to their defaults, it does not zero
+	/// out the program memory or the stack contents, but the stack pointers, top
+	/// of stack register, and the program counter.
+	pub fn reset(&mut self) {
+		self.pc = 0;
+		self.rp = RP0;
+		self.sp = SP0;
+		self.t  = 0;
+	}
+
+	/// Turns logging on/off, capturing each VM instructions execution
+	/// 
+	/// # Arguments
+	///
+	/// * `state` - Turn _very_ verbose tracing on/off, each instruction is logged to stderr in CSV format
+	///
+	pub fn trace(&mut self, state: bool)
+	{
+		self.tracing = state;
 	}
 
 	/// `run` executes the virtual machine on the currently loaded program
@@ -57,6 +141,13 @@ impl VM {
 	/// * `input`  - Input file to read from
 	/// * `output` - Output file to write to
 	/// * `block`  - Optional name of file to write sections of memory to
+	///
+	/// # Returns
+	///
+	/// This function returns an error code suitable for use with 
+	/// `std::process:exit()`, negative values usually indicate failure, however
+	/// any semantics attached to this number are entirely by convention only,
+	/// the program running in the virtual machine can return any number it likes.
 	///
 	/// # Example
 	///
@@ -71,7 +162,7 @@ impl VM {
 	/// use std::path::Path;
 	/// 
 	/// let mut evm = embed::VM::new();
-	/// let mut file = File::open(&Path::new("eforth.blk")).unwrap();
+	/// let mut file = File::open(&Path::new("vm.blk")).unwrap();
 	/// evm.load(&mut file);
 	/// evm.run(Some("new.blk"), &mut std::io::stdin(), &mut std::io::stdout());
 	/// ```
@@ -81,11 +172,13 @@ impl VM {
 		let mut d: u32;
 		let mut m = self.core;
 
+		VM::header(self, &mut std::io::stderr());
+
 		'eval: loop {
 			let instruction = m[pc as usize];
 			const DELTA: [u16; 4] = [0, 1, 0xfffe, 0xffff];
 
-			if self.trace { VM::trace(self, &mut std::io::stderr(), pc, instruction, t, sp, rp) }
+			VM::csv(self, &mut std::io::stderr(), pc, instruction, t, sp, rp);
 
 			if 0x8000 & instruction == 0x8000 { /* literal */
 				sp += 1;
@@ -122,8 +215,8 @@ impl VM {
 					20 => { sp = t >> 1 }
 					21 => { rp = t >> 1; tp = n }
 					22 => { tp = VM::save_file(self, block, n >> 1, (((t as u32) + 1) >> 1) as u16) } 
-					23 => { tp = VM::fputc(output, t as u8) } 
-					24 => { tp = VM::fgetc(input) }
+					23 => { tp = fputc(output, t as u8) } 
+					24 => { tp = fgetc(input) }
 					25 => { if t != 0 { tp = n / t; t = n % t; n = t } else { pc = 1; tp = 10 } }
 					26 => { 
 						if t != 0 { 
@@ -162,22 +255,51 @@ impl VM {
 		(t as i16) as i32
 	}
 
-	/// TODO: Generate output consumable by csv2vcd, which can then be viewed by GTKwave
-	/// See <https://github.com/carlos-jenkins/csv2vcd> and <http://gtkwave.sourceforge.net/>
-	fn trace(&self, output: &mut Write, pc: u16, instruction: u16, t: u16, sp: u16, rp: u16) -> () {
-		let _ignore = writeln!(output, "{}", format!("{:04x}, {:04x}, {:04x}, {:02x}, {:02x}", pc, instruction, t, sp, rp));
+	/// Print a header for a CSV file trace, if tracing is enabled, the output should be consumable
+	/// by the utility <https://github.com/carlos-jenkins/csv2vcd> which can turn a CSV file into
+	/// a VCD (Value Change Dump) file. This file can be used with a suitable waveform viewer, such
+	/// as GTKWave <http://gtkwave.sourceforge.net/> for debugging purposes. This is not a generic
+	///
+	fn header(&self, output: &mut Write) {
+		if !self.tracing { return }
+		let _ignore = writeln!(output, "\"pc[15:0]\",\"instruction[15:0]\",\"t[15:0]\",\"sp[7:0]\",\"rp[7:0]\",\"TIME\"");
 	}
 
-	fn fputc(output: &mut Write, t: u8) -> u16 {
-		let u: [u8; 1] = [t as u8];
-		if 1 == output.write(&u).unwrap() { t as u16 } else { 0xffff }
+	/// `csv` is used by `run` to output a CSV file with one line per instruction cycle,
+	/// it is for internal use only. Tracing has to be enabled and is off by default as it
+	/// produces a lot of output. The output should be compatible with the tool csv2vcd
+	/// [csv2vcd](https://github.com/carlos-jenkins/csv2vcd) and which can be viewed with
+	/// [GTKWave](http://gtkwave.sourceforge.net/), which should aid in analyzing the copious
+	/// amounts of data produced.
+	///
+	/// It should be noted that `csv` accepts the arguments it will print instead of printing
+	/// out the values stored in `self`, as the value for the VM state such as the program
+	/// counter and stack pointers are kept in locals until `run` returns, and only then are
+	/// they updated.
+	/// 
+	/// Arguments are logged in order, `pc` being the left most field in a record line and
+	/// `rp` the rightmost (of the values passed in, the rightmost field is actually a "time"
+	/// field, needed for the VCD format).
+	/// 
+	/// # Arguments
+	/// 
+	/// * `output`       - output stream to log to
+	/// * `pc`           - the program counter
+	/// * `instruction`  - the current instruction being executed, or `self->core[pc]`
+	/// * `t`            - top of stack register
+	/// * `sp`           - variable stack pointer, index into `core`
+	/// * `rp`           - return stack pointer, index into `core`
+	/// 
+	/// 
+	fn csv(&mut self, output: &mut Write, pc: u16, instruction: u16, t: u16, sp: u16, rp: u16) -> () {
+		if !self.tracing { return }
+		let time = if self.count == 0 { "s" } else { "ns" };
+		let _ignore = writeln!(output, "{}", format!("{:04x},{:04x},{:04x},{:02x},{:02x},{}{}", pc, instruction, t, sp, rp, self.count, time));
+		self.count += 1;
 	}
 
-	fn fgetc(input: &mut Read) -> u16 {
-		let mut u: [u8; 1] = [0];
-		if 1 == input.read(&mut u).unwrap() { u[0] as u16 } else { 0xffff }
-	}
-
+	/// `save_file` is for internal use only, as it converts any errors into results understandable
+	/// by the virtual machine. Its purpose is to save optionally save
 	fn save_file(&self, block: Option<&str>, start: u16, length: u16) -> u16 {
 		let name = match block { None => return 0xffff, Some(name) => name };
 
@@ -214,9 +336,17 @@ impl VM {
 	///
 	/// `output` - Output sink to write to, usually a file
 	///
-	/// # Example 
+	/// # Example
 	///
-	/// TODO: Add default image and example saving it to disk
+	/// ```
+	/// use std::fs::File;
+	/// use std::path::Path;
+	/// let mut vm = embed::VM::new();
+	/// let mut output = File::create(&Path::new("vm.blk")).unwrap();
+	/// vm.save(&mut output);
+	/// ```
+	///
+	/// TODO: Replace Option with proper Result return value
 	pub fn save(&self, output: &mut Write) -> Option<u16> {
 		VM::save_block(self, output, 0, CORE_SIZE as u16)
 	}
@@ -231,28 +361,58 @@ impl VM {
 	/// # Example
 	///
 	/// ```
-	/// use embed::VM;
 	/// use std::fs::File;
 	/// use std::path::Path;
-	/// let mut evm = embed::VM::new();
-	/// let mut file = File::open(&Path::new("eforth.blk")).unwrap();
-	/// evm.load(&mut file);
+	/// let mut vm = embed::VM::new();
+	/// let mut input = File::open(&Path::new("vm.blk")).unwrap();
+	/// vm.load(&mut input);
 	/// ```
 	///
+	/// TODO: Replace Option with proper Result return value
 	pub fn load(&mut self, input: &mut Read) -> Option<u16> {
 		let mut i = 0 as u16;
-		self.pc = 0;
-		self.t = 0;
-		self.rp = RP0;
-		self.sp = SP0;
+		self.reset();
 		while i < (CORE_SIZE as u16) {
-			let lo = VM::fgetc(input);
-			let hi = VM::fgetc(input);
+			let lo = fgetc(input);
+			let hi = fgetc(input);
 			if lo == 0xffff || hi == 0xffff { return Some(i) }
 			self.core[i as usize] = lo | (hi << 8);
 			i += 1
 		};
 		Some(i)
 	}
+
+
 }
 
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	const BYE: u16 = 0x7b00;
+	const ADD: u16 = 0x6523;
+
+	fn literal(l: u16) -> u16 {
+		if l & 0x8000 == 0x8000 { panic!("invalid literal {} > 0x7fff", format!("{}", l)) };
+		l | 0x8000
+	}
+
+	#[test]
+	fn run() {
+		let mut vm = VM::new();
+
+		//vm.core.copy_from_slice(&[literal(99), BYE]);
+		vm.core[0] = literal(99);
+		vm.core[1] = BYE;     
+		assert_eq!(vm.run(None, &mut ::std::io::stdin(), &mut ::std::io::stdout()), 99);
+		vm.reset();
+
+		vm.core[0] = literal(2); 
+		vm.core[1] = literal(2); 
+		vm.core[2] = ADD; 
+		vm.core[3] = BYE;   
+		assert_eq!(vm.run(None, &mut ::std::io::stdin(), &mut ::std::io::stdout()), 4);
+		vm.reset();
+
+	}
+}
--- main.rs
+++ main.rs
@@ -1,11 +1,12 @@
 extern crate embed;
 
-use std::fs::File;
+/*use std::fs::File;
 use std::path::Path;
-use std::env;
+use std::env;*/
 
 fn main()
 {
+	/*
 	let args: Vec<_> = env::args().collect();
 	if args.len() != 2 && args.len() != 3 {
 		panic!("Usage: {} vm.blk new.blk?");
@@ -15,8 +16,9 @@ fn main()
 
 	let mut vm = embed::VM::new();
 	let mut file = File::open(&Path::new(&args[1])).unwrap();
-	vm.load(&mut file);
+	vm.load(&mut file); */
 
-	std::process::exit(vm.run(Some(new), &mut std::io::stdin(), &mut std::io::stdout()));
+	let mut vm = embed::VM::new();
+	std::process::exit(vm.run(None, &mut std::io::stdin(), &mut std::io::stdout()));
 }
 
